80f53d99d44c5ffc236282766f05dec57c9f3163
Marek Veselý <veselym@spstrutnov.cz>
2023-11-27 18:34:45 +0100

    feat: yay downloads!

--- src/api/book.rs
+++ src/api/book.rs
@@ -1,26 +1,127 @@
 use crate::config::Config;
 // use hex_literal::hex;
-use crate::AuthData;
+use crate::{AuthData, ErrorResponse, Response};
 use actix_multipart::form::{tempfile::TempFile, MultipartForm};
-use actix_web::put;
-use actix_web::web;
+use actix_web::http::header::ContentDisposition;
+use actix_web::{delete, get, put};
+use actix_web::{error, web, HttpResponse};
+use chrono::format::format;
 use entity::book::ActiveModel as BookActiveModel;
+use entity::book::Column as BookCol;
+use entity::book::Model as BookModel;
 use entity::file_type::ActiveModel as FTActiveModel;
 use entity::file_type::Column as FTCol;
+use entity::file_type::Model as FTModel;
 use entity::prelude::Book;
 use entity::prelude::FileType;
 // use entity::user::{self, ActiveModel, Entity};
+use actix_files::NamedFile;
 use hex::encode;
-use sea_orm::{ActiveValue, ColumnTrait, DatabaseConnection, EntityTrait, QueryFilter};
-
+use sea_orm::{ActiveValue, ColumnTrait, DatabaseConnection, EntityTrait, Linked, QueryFilter};
+use serde::{Deserialize, Serialize};
 use sha2::{Digest, Sha256};
 use std::io::{Read, Write};
 use std::path::Path;
+#[derive(Deserialize)]
+struct BookId {
+    book_id: i32,
+}
 
+#[derive(Serialize)]
+struct FullBook {
+    pub id: i32,
+    pub title: String,
+    pub hash: String,
+    pub user_id: i32,
+    pub file_type: FTModel,
+}
+
+impl BookId {
+    pub async fn get(&self, uid: i32, pool: &DatabaseConnection) -> Result<FullBook, String> {
+        match Book::find_by_id(self.book_id)
+            .filter(BookCol::UserId.eq(uid))
+            .one(pool)
+            .await
+        {
+            Ok(Some(book)) => {
+                let ft = FileType::find_by_id(book.file_tyoe)
+                    .one(pool)
+                    .await
+                    .unwrap()
+                    .unwrap();
+                Ok(FullBook {
+                    file_type: ft,
+                    id: book.id,
+                    title: book.title,
+                    hash: book.hash,
+                    user_id: book.user_id,
+                })
+            }
+            Ok(None) => Err("No such book found.".to_string()),
+            Err(e) => Err(e.to_string()),
+        }
+    }
+}
 #[derive(Debug, MultipartForm)]
 struct UploadForm {
     #[multipart(rename = "file")]
     book: TempFile,
+}
+
+#[get("/book/{book_id}")]
+async fn book_info(
+    bookid: web::Path<BookId>,
+    db: web::Data<DatabaseConnection>,
+    AuthData(user): AuthData,
+) -> actix_web::Result<impl actix_web::Responder> {
+    match bookid.get(user.id, &db).await {
+        Ok(b) => Ok(HttpResponse::Ok().json(Response {
+            status: "ok".to_string(),
+            data: b,
+        })),
+        Err(e) => Err(error::ErrorNotFound(ErrorResponse {
+            status: "error".to_string(),
+            error: e,
+        })),
+    }
+}
+
+#[get("/book/{book_id}/dl")]
+async fn download(
+    bookid: web::Path<BookId>,
+    config: web::Data<Config>,
+    db: web::Data<DatabaseConnection>,
+    AuthData(user): AuthData,
+) -> NamedFile {
+    let book = bookid.get(user.id, &db).await.unwrap(); // we dont care just fail lol
+    let fp = format!("{}/{}.bin", config.filepath, book.hash);
+    NamedFile::open_async(fp)
+        .await
+        .unwrap()
+        .set_content_disposition(ContentDisposition::attachment(format!(
+            "{}.{}",
+            book.title, book.file_type.name
+        )))
+}
+
+#[delete("/book/{id}")]
+async fn remove(
+    config: web::Data<Config>,
+    db: web::Data<DatabaseConnection>,
+    AuthData(user): AuthData,
+) -> impl actix_web::Responder {
+    todo!("make it work");
+    ""
+}
+
+#[get("/books")]
+async fn list(
+    config: web::Data<Config>,
+    db: web::Data<DatabaseConnection>,
+    AuthData(user): AuthData,
+) -> impl actix_web::Responder {
+    todo!("make it work");
+    ""
 }
 
 #[put("/book")]
@@ -93,5 +194,9 @@ async fn upload(
 }
 
 pub fn configure(cfg: &mut web::ServiceConfig) {
-    cfg.service(upload);
+    cfg.service(upload)
+        .service(download)
+        .service(remove)
+        .service(list)
+        .service(book_info);
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,5 @@
+use std::fmt::Display;
+
 use actix_web::FromRequest;
 use actix_web::HttpMessage;
 use actix_web::HttpRequest;
@@ -22,7 +24,11 @@ pub struct ErrorResponse {
     status: String,
     error: String,
 }
-
+impl Display for ErrorResponse {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        serde_json::to_string(self).unwrap().fmt(f)
+    }
+}
 #[derive(Clone, Debug)]
 pub struct AuthData(pub entity::user::Model);
 
